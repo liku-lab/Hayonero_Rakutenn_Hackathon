e44c2981ffab5466f3861b480131587e351cb705
liku-lab <72571149+liku-lab@users.noreply.github.com>
2022-09-18 14:24:49 +0900

    Delete team3-main directory

--- team3-main/app/src/main/java/com/example/hayonero/database/PointDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.hayonero.database;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {PointData.class}, version = 1, exportSchema = false)
-public abstract class PointDatabase extends RoomDatabase {
-    public abstract PointDao pointDao();
-}
